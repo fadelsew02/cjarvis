e7b166ff5a466549725624cfe371a0cbda2bdbbd
koder <amedeflorianktm@gmail.com>
2024-07-21 15:50:06 +0100

    CJARVIS API

--- BackendCJARVIS/BackendCJARVIS/settings.py
+++ BackendCJARVIS/BackendCJARVIS/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cjarvisapi',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
